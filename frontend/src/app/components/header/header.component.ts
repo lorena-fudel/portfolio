1eeefa31a3cdc2f1527318e975730c009a0d4981
lorena <fmrlorena@gmail.com>
2026-03-09 13:29:16 +0000

    fix/modo-oscuro: modo ocuro implementado. fix/caja-carrusel: he implementado una caja en el carrusel para mejorar la visualizacion de las fotos

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
@@ -9,9 +9,14 @@ import { RouterModule } from '@angular/router';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   isScrolled = false;
   isMenuOpen = false;
+  isDarkMode = false;
+
+  ngOnInit() {
+    this.checkThemePreference();
+  }
 
   @HostListener('window:scroll', [])
   onWindowScroll() {
@@ -25,4 +30,27 @@ export class HeaderComponent {
   closeMenu() {
     this.isMenuOpen = false;
   }
+
+  checkThemePreference() {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme === 'dark' || (!savedTheme && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+      this.isDarkMode = true;
+      document.body.classList.add('dark-theme');
+    } else {
+      this.isDarkMode = false;
+      document.body.classList.remove('dark-theme');
+    }
+  }
+
+  toggleTheme() {
+    this.isDarkMode = !this.isDarkMode;
+    if (this.isDarkMode) {
+      document.body.classList.add('dark-theme');
+      localStorage.setItem('theme', 'dark');
+    } else {
+      document.body.classList.remove('dark-theme');
+      localStorage.setItem('theme', 'light');
+    }
+  }
 }
+
